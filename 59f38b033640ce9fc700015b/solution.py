b898cf8e096e253ffa87677ffd81bbc5260b5ddb
ibrahim <iercetin44@gmail.com>
2020-07-31 07:53:26 +0300

    Sum of prime-indexed elements

---
+++ 59f38b033640ce9fc700015b/solution.py
@@ -0,0 +1,18 @@
+# Sum of prime-indexed elements
+# https://www.codewars.com/kata/59f38b033640ce9fc700015b/train/python
+
+import math
+def is_prime(n):
+    if n % 2 == 0 and n > 2: 
+        return False
+    return all(n % i for i in range(3, int(math.sqrt(n)) + 1, 2))
+
+def total(arr):
+    l = len(arr)
+    result = 0
+    for i in range(2,l):
+        if is_prime(i):
+            #print("Num: {}, IsP: {}, To: {}".format(i,is_prime(i),arr[i]))
+            result += arr[i]
+
+    return result
